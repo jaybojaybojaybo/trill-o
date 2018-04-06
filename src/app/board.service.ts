ddc668bb85aabb31bf343ce1eefa783fec92a891
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-04-06 16:37:09 -0700

    make lists for specific boards finally display.

--- src/app/board.service.ts
+++ src/app/board.service.ts
@@ -3,6 +3,9 @@ import { Board } from './board';
 import { List } from './list';
 import { ListService } from './list.service';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import * as firebase from 'firebase';
+
+const db = AngularFireDatabase;
 
 @Injectable()
 export class BoardService {
@@ -40,23 +43,8 @@ export class BoardService {
 
   //Lists Methods
   getLists(boardId: string){
-    let boardsListsList = this.database.list('boards-lists'); //generates the boards-lists table associations boardId as key, listId as value
-    let boardsListsRef = this.database.database.ref('boards-lists'); //this references the exact path of the boards-lists table
-    console.log(boardsListsRef.toString());
-    let listIds = []; //this will be the destination for all listIds that are retrieved from the following method.
+    let boardsListsList = this.database.list('boards-lists/' + boardId); //generates the boards-lists table associations boardId as key, listId as value
 
-    let keys = boardsListsRef.on('child_added', function(snapshot){
-      listIds.push(snapshot.val());
-      console.log(snapshot.val());
-    });
-    console.log("this is listIds: " +listIds); //this currently returns all objects in boards-lists - how to get the values?
-    
-    listIds.forEach(function(listId){
-      let newList = this.listService.getListById(listId);
-      this.lists.push(newList);
-    })
-    console.log("this is this.lists: " + this.lists);
-    
-    return this.lists;
+    return boardsListsList;
   }
 }
