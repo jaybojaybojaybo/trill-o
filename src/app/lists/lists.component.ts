f96906691047f0cf3ae1847c1a3f4374f217378b
jaybojaybojaybo <jasun.feddema@gmail.com>
2018-04-06 09:20:14 -0700

    add functionality to pull list information from firebase and display it on a board-view component

--- src/app/lists/lists.component.ts
+++ src/app/lists/lists.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Board } from '../board';
+import { BoardService } from '../board.service';
+import { List } from '../list';
+import { ListService } from '../list.service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
-  styleUrls: ['./lists.component.css']
+  styleUrls: ['./lists.component.css'],
+  providers: [BoardService, ListService]
 })
 export class ListsComponent implements OnInit {
+  lists: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(
+    private router: Router, 
+    private boardService: BoardService, 
+    private listService: ListService
+  ) { }
 
   ngOnInit() {
+    this.lists = this.listService.getLists();
   }
 
 }
